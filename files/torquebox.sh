e982aa83892d30c486271416b65b1dbca4a4bec6
Nicholas Cancelliere <ncancelliere@gmail.com>
2012-06-29 09:44:12 -0500

    added jruby/bin to path

--- files/torquebox.sh
+++ files/torquebox.sh
@@ -2,3 +2,4 @@ export TORQUEBOX_HOME=/opt/torquebox/current
 export JBOSS_HOME=$TORQUEBOX_HOME/jboss
 export JRUBY_HOME=$TORQUEBOX_HOME/jruby
 export JRUBY_OPTS=--1.9
+export PATH=$JRUBY_HOME/bin:$PATH
